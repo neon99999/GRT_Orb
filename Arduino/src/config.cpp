1ad8cf651173bef6a1db142870807c6e40e897fd
neon99999 <matthew@matthewwaddell.me>
2026-01-23 00:32:15 -0700

    Update config.cpp

--- Arduino/src/config.cpp
+++ Arduino/src/config.cpp
@@ -7,7 +7,7 @@ enum Profile
   Studio,
   Theatre
 };
-static Profile ACTIVE_PROFILE = Studio;
+static Profile ACTIVE_PROFILE = Theatre;
 
 // Declarations
 const char *WIFI_SSID;
@@ -31,7 +31,7 @@ uint8_t RUN_MODE = MODE_PERPIXEL;
 static void loadStudio()
 {
   WIFI_SSID = "flubbyLindo";
-  WIFI_PASSWORD = "LaIslaSol2022!";
+  WIFI_PASSWORD = "xxxxxxxxxx";
 
   E131_UNIVERSE = 1;
   START_ADDR = 321;
